e540165c027a0ae2075bec1d305589e02ca4e04c
Sandeep Bhardwaj <sandeep.bhardwaj@gojek.com>
2022-06-28 23:36:04 +0530

    add root cmd

---
+++ cmd/root.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+const longDescription = `
+    __  ___  _                                
+   /  |/  / (_) _____  ____ _  ____ _  ___ 
+  / /|_/ / / / / ___/ / __  / / __  / / _ \
+ / /  / / / / / /    / /_/ / / /_/ / /  __/
+/_/  /_/ /_/ /_/     \__,_/  \__, /  \___/ 
+                            /____/         
+`
+
+func Execute() {
+	rootCmd := &cobra.Command{
+		Use:   "mirage",
+		Short: "An alternative cli to interact with optimus",
+		Long:  longDescription,
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
